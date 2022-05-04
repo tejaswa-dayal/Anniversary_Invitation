721de57bface79c9f806c09db21309b5ba42e8b3
Tejaswa Dayal <tejaswadayalsri@gmail.com>
2022-05-04 13:35:58 +0530

    First commit

---
+++ js/animation.js
@@ -0,0 +1,28 @@
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+  $(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".info-animation");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible-animation");
+      } else {
+        $(tag).removeClass("visible-animation");
+      }
+    }
+  });
